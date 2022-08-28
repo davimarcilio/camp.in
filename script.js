e03dfc9a71f03d340a8c0c439bfc84b7830292bf
Davi Marcilio <davimarcilio.js@gmail.com>
2022-08-28 16:45:51 -0300

    added password script functional

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ $('.inputTEXT').focusout((e) => {
     $(e.target).prevAll('.inputicon').eq(0).show();
     $(e.target).prevAll('.inputicon').eq(1).hide();
 });
+$('.eyepassword').click((e) => {
+    $(e.target).prevAll('.slash').show();
+    $(e.target).hide();
+    $(e.target).nextAll('.inputTEXT').attr('type', 'text');
+})
+$('.slash').click((e) => {
+    $(e.target).nextAll('.eyepassword').show();
+    $(e.target).hide();
+    $(e.target).nextAll('.inputTEXT').attr('type', 'password');
+})
